1e5add3e0ef46b39e1d00be3e1ac34036b946885
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-15 12:48:40 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -20,9 +20,14 @@ class Waiter
     Meal.all.select do |meal|
     meal.waiter == self
   end
-end
+  end
 
-  
+  def best_tipper
+    best_tipped_meal = meal.max do |meal_a,meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer
+  end   
   
 
 end
