d954f6e16caf0df2fb5f681755926fd83a73b734
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-15 12:45:11 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,20 @@
 class Waiter
+  attr_accessor :name, :yr_experience
+  
+  @@all = [ ]
+  
+  def initialize(name,yr_experience)
+    @name = name
+    @yr_experience = yr_experience
+    @@all << self
+  end 
+  def self.all
+    @@all
+  end
+  
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer,total,tip)
+  end 
+  
 
 end
